fe0b221bab248635c0012d6c806c8231f3516ede
Влад <49995385+EZNK@users.noreply.github.com>
2022-07-07 13:29:40 +0200

    ххахахапххапхахппхрахвррхава
    выпп

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
-const letters = ('0123456789QWERTYUIOPASDFGHJKLZXCVBNMqwertyuiopasdfghjklzxcvbnm~!@#$%^&*()+`":<>;\//')
+const letters = '0123456789QWERTYUIOPASDFGHJKLZXCVBNMqwertyuiopasdfghjklzxcvbnm~!@#$%^&*()+`":<>;\//'
 let pass = Math.random(letters).toString(36).slice(2, 20);
 document.getElementById("pass").innerHTML = pass;
 document.getElementById("button").onclick = function() {
-    const letters = ('0123456789QWERTYUIOPASDFGHJKLZXCVBNMqwertyuiopasdfghjklzxcvbnm~!@#$%^&*()+`":<>;\//')
 let pass = Math.random(letters).toString(36).slice(2, 20);
 document.getElementById("pass").innerHTML = pass;
 }
